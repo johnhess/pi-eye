1615d553bf05fa62aaeb52a96a89705705cb76fa
John Hess <mgojohn@gmail.com>
2018-01-14 22:21:58 -0500

    make traffic histogram from packet capture

--- src/pcap2json/hello/hello.go
+++ src/pcap2json/hello/hello.go
@@ -1,23 +1,147 @@
 package main
 
 import (
+    "bufio"
+    "encoding/json"
     "fmt"
-    "github.com/google/gopacket"
-    "github.com/google/gopacket/pcap"
+    "io/ioutil"
+    "os"
+    "strconv"
 )
 
-func read() {
-    fmt.Println("Attempting to parse pcap")
-    if handle, err := pcap.OpenOffline("/Users/johnhess/test.pcap"); err != nil {
-    // if handle, err := pcap.OpenOffline("/Users/johnhess/Downloads/dns.cap"); err != nil {
+type Ip struct {
+    Ip_ip_dst_host string
+    Ip_ip_dst string
+    Ip_ip_src_host string
+    Ip_ip_src string
+}
+
+func (ip Ip) String() string {
+    if ip.Ip_ip_dst_host != "" {
+        return fmt.Sprintf(
+            "IP Information: DST: %s SRC: %s", 
+            ip.Ip_ip_dst_host, 
+            ip.Ip_ip_src_host)
+    }
+    return ""
+}
+
+type Dns struct {
+    Text_dns_qry_name string
+}
+
+func (dns Dns) String() string {
+    if dns.Text_dns_qry_name != "" {
+        return fmt.Sprintf("DNS Information: %s", dns.Text_dns_qry_name)
+    }
+    return ""
+}
+
+type Tcp struct {
+    Tcp_analysis_tcp_analysis_bytes_in_flight string
+}
+
+func (tcp Tcp) String() string {
+    if tcp != (Tcp{}) {
+        return fmt.Sprintf(
+            "TCP Size: %s", 
+            tcp.Tcp_analysis_tcp_analysis_bytes_in_flight)
+    }
+    return ""
+}
+
+type Layers struct {
+    Dns Dns
+    Ip Ip
+    Tcp Tcp
+}
+
+type Packet struct {
+    Timestamp string
+    Layers Layers
+}
+
+func str2pkt(s string) Packet {
+    pkt := Packet{}
+    if err := json.Unmarshal([]byte(s), &pkt); err != nil {
         panic(err)
-    } else {
-        packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-        for packet := range packetSource.Packets() {
-            fmt.Println(packet)
+    }
+    return pkt;
+}
+
+type TrafChunk struct {
+    Timestamp int
+    Count int
+}
+
+func traffichist(pkts []Packet, delta int) []TrafChunk {
+    // Create and fill chunks as long as there are still packets in the array.
+    pkti := 0
+    var chunks []TrafChunk
+    // First chunk starts at the time of the first packet
+    offset, err := strconv.Atoi(pkts[0].Timestamp)
+    if err != nil {
+        panic(err)
+    }
+    for {
+        chunk := TrafChunk{offset, 0}
+        for {
+            // end of pkts
+            if pkti >= len(pkts) {break}
+            tm, err := strconv.Atoi(pkts[pkti].Timestamp)
+            if err != nil {
+                panic(err)
+            }
+            if tm < offset + delta {
+                chunk.Count++
+                pkti++
+            } else {break} // end of chunk
+        }
+        chunks = append(chunks, chunk)
+        offset += delta
+        // finally stop if we're out of packets
+        if pkti >= len(pkts) {break}
+    }
+    return chunks
+}
+
+func savehist(hist []TrafChunk, f string) {
+    out, err := json.Marshal(hist)
+    if err != nil {
+        panic(err)
+    }
+
+    err = ioutil.WriteFile(f, []byte(string(out)), 0644)
+    if err != nil {
+        panic(err)
+    }
+}
+
+func read() {
+    fmt.Println("Attempting to parse EK data from file.")
+    file, err := os.Open("/Users/johnhess/stream.ek")
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+
+    var pkts []Packet
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        line := scanner.Text()
+        pkt := str2pkt(line)
+        if (Ip{}) != pkt.Layers.Ip || (Dns{}) != pkt.Layers.Dns {
+            pkts = append(pkts, pkt)
         }
     }
 
+    if err := scanner.Err(); err != nil {
+        panic(err)
+    }
+
+    hist := traffichist(pkts, 100)
+    savehist(hist, "/Users/johnhess/Dropbox/hackamajig/networkviz/hist.json")
 }
 
 func returntwo() int {
@@ -25,6 +149,5 @@ func returntwo() int {
 }
 
 func main() {
-    fmt.Println("Hello, World.")
     read()
 }
--- src/pcap2json/hello/hello_test.go
+++ src/pcap2json/hello/hello_test.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+    "fmt"
     "testing"
 )
+
+var pkts = []Packet{
+    Packet{"1000000000000", Layers{}}, 
+    Packet{"1000000004001", Layers{}}, 
+    Packet{"1000000004001", Layers{}}, 
+    Packet{"1000000009001", Layers{}}}
 
 func TestTwo(t *testing.T) {
     two := returntwo()
     if two != 2 {
         t.Errorf("returned two but failing anyways")
+    }
+}
+
+func TestHist(t *testing.T) {
+    hist := traffichist(pkts, 1000)
+    if len(hist) != 10 {
+        t.Errorf(fmt.Sprintf("Wrong number of slices in hist: %d", len(hist)))
+    }
+    if hist[0].Count != 1 {
+        t.Errorf("first chunk should have seen one packet")
+    }
+    if hist[1].Count != 0 {
+        t.Errorf("second chunk should have seen no packets")
+    }
+    if hist[4] != (TrafChunk{1000000004000, 2}) {
+        t.Errorf("didnt catch double packet window")
     }
 }
